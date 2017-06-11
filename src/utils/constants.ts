dac90a70d4fbe4e1f7ad610554bd9af97d14257e
rizkys@gitlab.com <rizky.solechudin@gmail.com>
2017-06-11 13:39:40 +0700

    fixed api base url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,10 +8,10 @@ export const APP_BASE_URL = "http://192.168.2.119";
 export const APP_BASE_PORT = ":8100";
 export const APP_URL = `${APP_BASE_URL}${APP_BASE_PORT}`;
 
-export const API_BASE_URL = "http://localhost";
-export const API_BASE_PORT = ":8085";
+// export const API_BASE_URL = "http://localhost";
+// export const API_BASE_PORT = ":8085";
 
-// export const API_BASE_URL = "http://jalan-belakang.herokuapp.com";
-// export const API_BASE_PORT = "";
+export const API_BASE_URL = "http://jalan-belakang.herokuapp.com";
+export const API_BASE_PORT = "";
 
 export const API_URL = `${API_BASE_URL}${API_BASE_PORT}`;
